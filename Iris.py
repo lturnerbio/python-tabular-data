775f760e4b94fb85674c84de4127c1f346087139
Logan Turner <lturnerbio@gmail.com>
2021-03-23 11:47:05 -0500

    Uploaded figures and updated script that should be executable

--- Iris.py
+++ Iris.py
@@ -5,15 +5,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
-if __name__ == '__main__'
+
 
 dataframe = pd.read_csv("iris.csv")
 
 
+versicolor = dataframe[dataframe.species == 'Iris_versicolor']
+x = versicolor.petal_length_cm
+y = versicolor.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.legend()
+plt.savefig("petal_v_sepal_length_regress_versicolor.png")
 
-versicolor = dataframe[dataframe.species = 'Iris_versicolor']
 
-virginica = dataframe[dataframe.species = 'Iris_virginica']
 
-setosa = dataframe[dataframe.species = 'Iris_setosa']
+virginica = dataframe[dataframe.species == 'Iris_virginica']
+x = virginica.petal_length_cm
+y = virginica.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.legend()
+plt.savefig("petal_v_sepal_length_regress_virginica.png")
 
+
+
+setosa = dataframe[dataframe.species == 'Iris_setosa']
+x = setosa.petal_length_cm
+y = setosa.sepal_length_cm
+regression = stats.linregress(x, y)
+slope = regression.slope
+intercept = regression.intercept
+plt.scatter(x, y, label = 'Data')
+plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal length (cm)")
+plt.legend()
+plt.savefig("petal_v_sepal_length_regress_setosa.png")
