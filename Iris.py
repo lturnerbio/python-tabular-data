d9a4045bac8f3b228fbd73fad30b2f94b18f5f56
Logan Turner <lturnerbio@gmail.com>
2021-03-23 11:30:19 -0500

    file now executable, new lines added. still not functionable

--- Iris.py
+++ Iris.py
@@ -1,8 +1,19 @@
-#test message
+#! /usr/bin/env python3
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
+if __name__ == '__main__'
 
+dataframe = pd.read_csv("iris.csv")
+
+
+
+versicolor = dataframe[dataframe.species = 'Iris_versicolor']
+
+virginica = dataframe[dataframe.species = 'Iris_virginica']
+
+setosa = dataframe[dataframe.species = 'Iris_setosa']
 
